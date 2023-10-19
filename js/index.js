95c8acb963da1b0298794036c66dfca17a96b677
Vitor Domingos <vm486092@gmail.com>
2023-10-18 22:12:40 -0200

    adicionada a pasta js

---
+++ js/index.js
@@ -0,0 +1,30 @@
+
+// Declarando a variável 'name' e atribuindo o valor "Elias"
+let name = "Elias"
+
+// Declarando a variável 'number' e atribuindo o valor 0.2222222222 (como número, não como string)
+let number = 0.2222222222
+
+// Declarando a variável 'usuarioVerificado' e atribuindo o valor booleano 'false'
+let usuarioVerificado = false
+
+// Verifica se 'usuarioVerificado' é verdadeiro (true) e imprime a mensagem apropriada
+if (usuarioVerificado) {
+    console.log("O usuário é verificado")
+} else {
+    // Se 'usuarioVerificado' for falso (false), esta mensagem será impressa
+    console.log("O usuário não é verificado")
+}
+
+// A linha a seguir está comentada, portanto, a declaração da variável 'document' será ignorada.
+// let document = null
+
+// Declarando a variável 'email' e atribuindo o valor 'undefined' a ela
+let email = undefined
+
+// Imprime o valor da variável 'email', que é 'undefined'
+console.log(email)
+
+
+
+
